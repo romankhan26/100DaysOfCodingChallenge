c780bb7323d80360f89b284ac86e594cf92b0f90
roman khan <romankhan.26@outlook.com>
2024-04-05 17:42:34 +0500

    question 57, 58 ,59 and 60

---
+++ Day#19/Q57.ts
@@ -0,0 +1,13 @@
+// Question 57: Find the Average Grade: Given a list of grades, calculate the average grade.
+
+// Explain & TIP: We can sum up all the grades and divide by the number of grades to find the 
+//average, giving us an idea of how well someone did overall.
+
+let grades = [90, 76, 79, 98, 79, 87, 94, 59, 88, 77];
+let averageGrades = grades.reduce((total,grades) => total + grades , 0 )/ grades.length;
+console.log(averageGrades);
+if (averageGrades > 80 ){
+    console.log("GRADE : A1");
+    
+}//particularly here we have grade > 80, therefore we are not giving more if/else statements...
+
